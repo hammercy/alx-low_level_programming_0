567944db6f497da1886585956bf76ba3c49fa66f
Fesseha <dafisseha@outlook.com>
2023-01-02 10:50:14 -0800

    My second commit

--- 0x00-hello_world/main.c
+++ 0x00-hello_world/main.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main-Entry point
- *
+ * printf: export CFILE=0-main.c;
+ * printf:./0-preprocessor
  * Return: Always 0 (Success)
  */
 int main(void)
 {
 	printf("export CFILE=0-main.c \n");
 	printf("./0-preprocessor \n");
-	printf("tail c \n");
 	return (0);
 }
 
