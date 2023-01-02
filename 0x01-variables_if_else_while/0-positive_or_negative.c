06964ec83a19e208b044979e9330871a04706a46
Fesseha <dafisseha@outlook.com>
2023-01-02 15:18:08 -0800

    C variables if else while

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -6,12 +6,12 @@
 
 /**
  * main - evluate negative or positive
- * @n: a parameter to be checked
  *
  * Description: The function generate
  * a random number then checks
  * whether it is negative or positive
  * and prints the result.
+ *
  * return: 0 always when successful
  */
 
@@ -22,10 +22,10 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
-		printf("%d is positive\n", n);
+		printf("%d is positive\n",n);
 	else if (n < 0)
-		printf("%d is negative\n", n);
+		printf("%d is negative\n",n);
 	else
-		printf("%d is zero\n", n);		  
+		printf("%d is zero\n",n);		  
 	return (0);
 }
