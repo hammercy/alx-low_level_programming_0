6448f1e4fe76c360732d00cd1adf4a0984c511a6
Fesseha <dafisseha@outlook.com>
2023-01-02 16:45:16 -0800

    C variables if else while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints lower case alphabet
+ *
+ * Description: this function prints
+ * lower case alphabet from start to 
+ * finish
+ * Return: 0 always when successful
+ */
+int main(void)
+{
+	char ch;
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
